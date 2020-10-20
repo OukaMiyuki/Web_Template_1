c5f3fb40a81e0f3a0570f41e8121ac4d041214b1
OukaMiyuki <asukaloid.blog@gmail.com>
2020-10-20 11:33:19 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<!--[if lte IE 6]><html class="preIE7 preIE8 preIE9"><![endif]-->
+<!--[if IE 7]><html class="preIE8 preIE9"><![endif]-->
+<!--[if IE 8]><html class="preIE9"><![endif]-->
+<!--[if gte IE 9]><!--><html><!--<![endif]-->
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <title>Wordpress Theme1</title>
+        <meta name="author" content="name">
+        <meta name="description" content="description here">
+        <meta name="keywords" content="keywords,here">
+        <link rel="stylesheet" href="./assets/CSS/style.css" type="text/css">
+    </head>
+    <body>
+
+        <!-- Nav Part -->
+
+        <nav class="nav">
+            <div class="nav-menu flex-row">
+                <div class="nav-brand">
+                    <a href="" class="text-gray">Wordpress 1</a>
+                </div>
+                <div>
+                    <ul class="nav-items">
+                        <li class="nav-link">
+                            <a href="">Home</a>
+                        </li>
+                        <li class="nav-link">
+                            <a href="">Categfory</a>
+                        </li>
+                        <li class="nav-link">
+                            <a href="">Archive</a>
+                        </li>
+                        <li class="nav-link">
+                            <a href="">Pages</a>
+                        </li>
+                        <li class="nav-link">
+                            <a href="">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="social text-gray">
+                    <a href="">F</a>
+                    <a href="">I</a>
+                    <a href="">T</a>
+                    <a href="">Y</a>
+                </div>
+            </div>
+        </nav>
+
+        <!-- End Nav Part -->
+    
+        <script src="./assets/JS/main.js"></script>
+    </body>
+</html>
